7fc4f3d2008d6fe2d2cb6c9d87ef4aee62988d74
Salakksson <alexsalek2007@gmail.com>
2024-06-23 21:18:05 +0100

    fixed previous commit

--- install.sh
+++ install.sh
@@ -7,6 +7,8 @@ INSTALL_DIR="/usr/local/bin"
 
 xxd -i -n default_config ./src/default_config.ini src/default_config.h
 
+mkdir -p ./bin
+
 if gcc $CFLAGS ./src/*.c -o $TARGET;
 then
     cp $TARGET $INSTALL_DIR/$TARGET
@@ -14,5 +16,5 @@ else
     echo "Failed to compile $TARGET, won't install to $INSTALL_DIR"
 fi
 
-rm -fr ./bin/*.o    # .o extension in case someone accidentally removes
+# rm -fr ./bin/*.o    # .o extension in case someone accidentally removes
                     # proceding . and removes /bin if running with sudo
